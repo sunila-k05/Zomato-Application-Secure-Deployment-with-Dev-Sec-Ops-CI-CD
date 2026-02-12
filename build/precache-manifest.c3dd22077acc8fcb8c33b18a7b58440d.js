def811956b2666b9072cb6814a43825e8146a1a3
SURYAMAKES <sunil.devops777@gmail.com>
2026-02-12 10:50:35 +0530

    build changes

---
+++ build/precache-manifest.c3dd22077acc8fcb8c33b18a7b58440d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521f82f8e4f75128f431cda59d4c3310",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2c0c5a432de69f64ca5",
+    "url": "/static/css/main.40af8486.chunk.css"
+  },
+  {
+    "revision": "93492ddc1f651923327a",
+    "url": "/static/js/2.4748f581.chunk.js"
+  },
+  {
+    "revision": "3c29228dd4baaa7d5a4f34915131c285",
+    "url": "/static/js/2.4748f581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c0c5a432de69f64ca5",
+    "url": "/static/js/main.11c32e7a.chunk.js"
+  },
+  {
+    "revision": "bd7a7568036d407ad1bd",
+    "url": "/static/js/runtime-main.e6bd3e7c.js"
+  },
+  {
+    "revision": "e3029d12f6dc48b9e0e2d512ae09d771",
+    "url": "/static/media/App-Store.e3029d12.png"
+  },
+  {
+    "revision": "322fba9606c800951d6fe9aa222dddfd",
+    "url": "/static/media/BackGround.322fba96.png"
+  },
+  {
+    "revision": "8d49ce7e266a77095f49e93ff86302dd",
+    "url": "/static/media/Dine-Out.8d49ce7e.png"
+  },
+  {
+    "revision": "ecd5268b91db9ee0e9d852b571219daf",
+    "url": "/static/media/Google-Play.ecd5268b.png"
+  },
+  {
+    "revision": "475119753b88ec8e5dd8ed806d480ced",
+    "url": "/static/media/Iphone.47511975.png"
+  },
+  {
+    "revision": "7364793ef6faa1c8326600080a07320f",
+    "url": "/static/media/Night-Life.7364793e.png"
+  },
+  {
+    "revision": "158e724ae7d2b71a1e0fa6cdb4e6c32e",
+    "url": "/static/media/Online-Food.158e724a.png"
+  },
+  {
+    "revision": "b1f4ce99999fb6c773c77ba62eabcc2c",
+    "url": "/static/media/Zomato-Logo.b1f4ce99.png"
+  },
+  {
+    "revision": "e0bfd4e1a14eb7446cc9e2c63b1a3925",
+    "url": "/static/media/blackLogo.e0bfd4e1.webp"
+  },
+  {
+    "revision": "feb9cdf599c2ed0b7f3c26d52e1626a6",
+    "url": "/static/media/collection1.feb9cdf5.webp"
+  },
+  {
+    "revision": "8a043fb26cac7138ed7d664970e8f091",
+    "url": "/static/media/collection2.8a043fb2.webp"
+  },
+  {
+    "revision": "c4cab5a67858e73cce9493320663a06a",
+    "url": "/static/media/collection3.c4cab5a6.webp"
+  },
+  {
+    "revision": "bb7b9d55ba593ed70e07ac2ce87373b1",
+    "url": "/static/media/collection4.bb7b9d55.webp"
+  }
+]);
